0006e02049a2137713be169be92b2227544f8c8a
benhowarth6 <benhowarth6@gmail.com>
2021-02-15 17:02:22 +0000

    Revert "book-repair"
    This reverts commit 5dcd4a37390c5e6b2fd4b72d783581ccb513ba7d.

--- pages/iphone/book-repair.js
+++ pages/iphone/book-repair.js
@@ -36,7 +36,7 @@ export async function getStaticProps(context) {
 <FormiumForm
           data={form}
           onSubmit={async (values) => {
-            // Send form values to Formium Dashboard
+            // Send form values to Formium
             await formium.submitForm('book-repair', values);
             alert('Success');
           }}
