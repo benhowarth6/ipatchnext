9fdbb3651e554983db746143c9d377d1517468c4
Ben Howarth <benhowarth6@gmail.com>
2021-03-15 16:39:25 +0000

    Identify

--- pages/macRepair.js
+++ pages/macRepair.js
@@ -142,6 +142,21 @@ export default function Home() {
       </div>
     </div>
   </div>
+  <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+  <div className="bg-white rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
+        <div className="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
+          <div className="lg:self-center">
+            <h2 className="text-3xl font-extrabold sm:text-4xl">
+              <span className="block">Not sure which Mac you have?</span>
+            </h2>
+            <p className="mt-4 text-lg leading-6 text-gray-800">If your Mac powers on, choose Apple menu  > About This Mac.</p>
+          </div>
+        </div>
+        <div className="-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1">
+          <img className="transform translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20" src="mac/identify-mac.jpg" alt="App screenshot" />
+        </div>
+      </div>
+      </div>
 </div>
 <Footer />
 </div>
