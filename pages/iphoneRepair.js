66585714a2bc922f42b3df4e38d8535f416974e6
Ben Howarth <benhowarth6@gmail.com>
2021-03-26 13:53:22 +0000

    fixes

--- pages/iphoneRepair.js
+++ pages/iphoneRepair.js
@@ -30,7 +30,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 hidden lg:grid">
+      <div className="bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 hidden sm:grid">
         <div className="max-w-7xl mx-auto">
           <div className="text-center">
             <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
