bc27c3eb28400a16de850734a18edcc25a22e66d
Ben Howarth <benhowarth6@gmail.com>
2021-04-16 16:39:29 +0100

    sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,10 +1,4 @@
 module.exports = {
-  siteUrl: 'https://ipatchrepairs.co.uk',
-  generateRobotsTxt: true,
-  // optional
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      'https://ipatchrepairs.co.uk/my-custom-sitemap-1.xml',
-    ],
-  },
-}
+    siteUrl: 'https://www.ipatchrepairs.co.uk',
+    generateRobotsTxt: true,
+  }
