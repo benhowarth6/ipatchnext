d638b7eda78e45265bda5ce0e6d5eb53d08c0661
Ben Howarth <benhowarth6@gmail.com>
2021-06-16 12:13:31 +0100

    layout

--- pages/iphone-repairs.js
+++ pages/iphone-repairs.js
@@ -54,7 +54,7 @@ const features = [
 ]
 const metrics = [
   { id: 1, stat: '8K+', emphasis: 'Screen repairs', rest: 'completed.' },
-  { id: 2, stat: '25K+', emphasis: 'Countries around the globe', rest: 'lacus nibh integer quis.' },
+  { id: 2, stat: '25K+', emphasis: 'Batteries', rest: 'changed.' },
   { id: 3, stat: '98%', emphasis: 'Customer satisfaction', rest: 'laoreet amet lacus nibh integer quis.' },
   { id: 4, stat: '12M+', emphasis: 'Issues resolved', rest: 'lacus nibh integer quis.' },
 ]
