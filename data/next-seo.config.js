3f554b6b26a872d7979c6a1cb617d983409bf0b1
Ben Howarth <benhowarth6@gmail.com>
2021-07-13 14:20:13 +0100

    update seo-config

--- data/next-seo.config.js
+++ data/next-seo.config.js
@@ -1,6 +1,3 @@
 export default {
   titleTemplate: '%s | iPatch',
-  description:
-      "Expert Apple iPhone, iPad and MacBook repairs in Trinity Leeds. We offer screen replacements, new batteries and much more.",
-  canonical: "https://www.ipatchrepairs.co.uk",
 };
