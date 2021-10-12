2affb1bd9ca45d34a371a793b0f4853ed83b4b5d
Ben Howarth <benhowarth6@gmail.com>
2021-10-12 16:25:09 +0100

    Update Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -83,20 +83,6 @@ export default function Example() {
                   )
                 })}
               </div>
-
-
-              <div className="border-t border-gray-200 py-6 px-4 space-y-6">
-                <div className="flow-root">
-                  <a href="#" className="-m-2 p-2 block font-medium text-gray-900">
-                    Create an account
-                  </a>
-                </div>
-                <div className="flow-root">
-                  <a href="#" className="-m-2 p-2 block font-medium text-gray-900">
-                    Sign in
-                  </a>
-                </div>
-              </div>
             </div>
           </Transition.Child>
         </Dialog>
