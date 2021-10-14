43fcc1ba32c6d4434b6eb4b7169885b030b712c7
Ben Howarth <benhowarth6@gmail.com>
2021-10-14 11:09:28 +0100

    updates

--- pages/iphone-repairs.js
+++ pages/iphone-repairs.js
@@ -12,8 +12,7 @@ import About from "../components/iphone/About";
 import About2 from "../components/iphone/About2";
 import Parts from "../components/iphone/Parts";
 
-import iphoneModels from '../data/iphone/iphoneModels.json';
-import repairs from '../data/iphone/iphone-repairs.json';
+import models from '../data/iphone/iphone-models.json';
 
 export default function Home() {
   return (
@@ -40,168 +39,169 @@ export default function Home() {
       <Navigation />
 
       <div className="bg-gray-50">
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="py-24 text-center">
-              <h1 className="text-4xl font-extrabold tracking-tight text-gray-900">iPhone Repairs</h1>
-              <p className="mt-4 max-w-3xl mx-auto text-base text-gray-500">
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="py-24 text-center">
+            <h1 className="text-4xl font-extrabold tracking-tight text-gray-900">iPhone Repairs</h1>
+            <p className="mt-4 max-w-3xl mx-auto text-base text-gray-500">
               We offer a large selection of iPhone repairs including screens, batteries, charging ports and more for nearly all models.
-              </p>
-            </div>
-      </div>
-      </div>
-
-      <div className="bg-white">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="md:flex md:items-center md:justify-between">
-          <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Select your iPhone model</h2>
-          <a href="#" className="hidden text-sm font-medium text-blue-600 hover:text-blue-500 md:block">
-            Need help<span aria-hidden="true"> &rarr;</span>
-          </a>
-        </div>
-
-        <div className="mt-6 grid grid-cols-2 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
-    {iphoneModels.map(iphoneModel => {
-        const { id, name, image, alt, href } = iphoneModel;
-        return (
-            <div key={id} className="relative group">
-              <div className="aspect-w-3 aspect-h-3 rounded-lg overflow-hidden bg-gray-100">
-                <img src={image} alt={alt} className="object-center object-cover" />
-                <div className="flex items-end opacity-0 p-4 group-hover:opacity-100" aria-hidden="true">
-                  <div className="w-full bg-white bg-opacity-75 backdrop-filter backdrop-blur py-2 px-4 rounded-md text-sm font-medium text-gray-900 text-center">
-                    View Repairs
-                  </div>
-                </div>
-              </div>
-              <div className="mt-4 flex items-center justify-between text-base font-semibold text-gray-900 space-x-8">
-                <h3>
-                  <Link href={href}>
-                  <a>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    {name}
-                  </a>
-                  </Link>
-                </h3>
-              </div>
-            </div>
-          )})}
-        </div>
-
-        <div className="mt-8 text-sm md:hidden">
-          <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-            Shop the collection<span aria-hidden="true"> &rarr;</span>
-          </a>
-        </div>
-      </div>
-    </div>
-
-    <Incentive />
-    <About />
-    <About2 />
-    <Parts />
-
-      <div className="bg-white">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="max-w-2xl lg:mx-auto lg:text-center">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                Frequently asked questions
-              </h2>
-              <p className="mt-4 text-lg text-gray-500">
-                Can’t find the answer you’re looking for? Reach out to our{" "}
-                <Link href="contact">
-                  <a className="font-medium text-blue-600 hover:text-blue-500">
-                    customer support
-                  </a>
-                </Link>{" "}
-                team.
-              </p>
-            </div>
-            <div className="mt-20">
-          <dl className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-10">
-                <div>
-                  <dt className="text-lg leading-6 font-medium text-gray-900">
-                    How do I get my iPhone Repaired?
-                  </dt>
-                  <dd className="mt-2 text-base text-gray-500">
-                    Choose the repair your device requires and the best booking
-                    option for you. We accept both drop off and mail in repairs,
-                    so wether your local or not we can still offer you our
-                    services. If you aren’t sure which repair your iPhone
-                    requires, please get in touch and we’ll do our best to help.
-                  </dd>
-                </div>
-
-                <div>
-                  <dt className="text-lg leading-6 font-medium text-gray-900">
-                    How much does it cost?
-                  </dt>
-                  <dd className="mt-2 text-base text-gray-500">
-                    The cost for your repair will depend on the type of repair
-                    and the model of iPhone. We list nearly all repair costs for
-                    the the iPhones we offer service on. Should you not be able
-                    to find your repair listed, please get in touch and we’ll be
-                    able to advise further. For liquid damage devices, or no
-                    power devices we wouldn’t be able to provide a repair quote
-                    until after an inspection.
-                  </dd>
-                </div>
-
-                <div>
-                  <dt className="text-lg leading-6 font-medium text-gray-900">
-                    How long will it take?
-                  </dt>
-                  <dd className="mt-2 text-base text-gray-500">
-                    We understand how important iPhones have become, so we aim
-                    to complete all repairs in the same day, usually the same
-                    hour. A drop off repair for your iPhone is the quickest way
-                    to get your device repaired, and we usually complete nearly
-                    all iPhone repairs in under 30 minutes. Should you want to
-                    mail in instead, we aim to repair and dispatch the device
-                    back to you within the same day.
-                  </dd>
-                </div>
-
-                <div>
-                  <dt className="text-lg leading-6 font-medium text-gray-900">
-                    What to expect?
-                  </dt>
-                  <dd className="mt-2 text-base text-gray-500">
-                    We offer a wide range of repairs for all iPhone models,
-                    however should a repair not be possible on your device you
-                    can collect your iPhone with no fee or just the return
-                    postage fee if you’ve mailed in. Although we don’t routinely
-                    erase any device during repair, we do recommend you take a
-                    backup of your data before having your device repaired.
-                  </dd>
-                </div>
-
-                <div>
-                  <dt className="text-lg leading-6 font-medium text-gray-900">
-                    Screen Replacements
-                  </dt>
-                  <dd className="mt-2 text-base text-gray-500">
-                    iPhone screen replacements are our most common repair, we
-                    can repair most screens within around 20 minutes. We aim to
-                    keep all models of display in stock so you can pop into our
-                    Leeds store for a quick replacement without an appointment.
-                  </dd>
-                </div>
-
-                <div>
-                  <dt className="text-lg leading-6 font-medium text-gray-900">
-                    Battery Replacements
-                  </dt>
-                  <dd className="mt-2 text-base text-gray-500">
-                    iPhone batteries have a limited life span, this is usually
-                    around 1000 full charges. If your battery no longer gets you
-                    through the day we can offer a quick replacement, usually
-                    within 15 minutes. We keep nearly all batteries in stock, so
-                    you can stop by for a quick replacement at our Leeds store.
-                  </dd>
-                </div>
-              </dl>
-            </div>
+            </p>
           </div>
         </div>
+      </div>
+
+      <div className="bg-white">
+        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="md:flex md:items-center md:justify-between">
+            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Select your iPhone model</h2>
+            <a href="#" className="hidden text-sm font-medium text-blue-600 hover:text-blue-500 md:block">
+              Need help<span aria-hidden="true"> &rarr;</span>
+            </a>
+          </div>
+
+          <div className="mt-6 grid grid-cols-2 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
+            {models.map(models => {
+              const { id, name, image, alt, href, slug } = models;
+              return (
+                <div key={id} className="relative group">
+                  <div className="aspect-w-3 aspect-h-3 rounded-lg overflow-hidden bg-gray-100">
+                    <img src={image} alt={alt} className="object-center object-cover" />
+                    <div className="flex items-end opacity-0 p-4 group-hover:opacity-100" aria-hidden="true">
+                      <div className="w-full bg-white bg-opacity-75 backdrop-filter backdrop-blur py-2 px-4 rounded-md text-sm font-medium text-gray-900 text-center">
+                        View Repairs
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-4 flex items-center justify-between text-base font-semibold text-gray-900 space-x-8">
+                    <h3>
+                      <Link href={`/iphone-repairs/${id}`}>
+                        <a>
+                          <span aria-hidden="true" className="absolute inset-0" />
+                          {name}
+                        </a>
+                      </Link>
+                    </h3>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+
+          <div className="mt-8 text-sm md:hidden">
+            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+              Shop the collection<span aria-hidden="true"> &rarr;</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <Incentive />
+      <About />
+      <About2 />
+      <Parts />
+
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="max-w-2xl lg:mx-auto lg:text-center">
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Frequently asked questions
+            </h2>
+            <p className="mt-4 text-lg text-gray-500">
+              Can’t find the answer you’re looking for? Reach out to our{" "}
+              <Link href="contact">
+                <a className="font-medium text-blue-600 hover:text-blue-500">
+                  customer support
+                </a>
+              </Link>{" "}
+              team.
+            </p>
+          </div>
+          <div className="mt-20">
+            <dl className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-10">
+              <div>
+                <dt className="text-lg leading-6 font-medium text-gray-900">
+                  How do I get my iPhone Repaired?
+                </dt>
+                <dd className="mt-2 text-base text-gray-500">
+                  Choose the repair your device requires and the best booking
+                  option for you. We accept both drop off and mail in repairs,
+                  so wether your local or not we can still offer you our
+                  services. If you aren’t sure which repair your iPhone
+                  requires, please get in touch and we’ll do our best to help.
+                </dd>
+              </div>
+
+              <div>
+                <dt className="text-lg leading-6 font-medium text-gray-900">
+                  How much does it cost?
+                </dt>
+                <dd className="mt-2 text-base text-gray-500">
+                  The cost for your repair will depend on the type of repair
+                  and the model of iPhone. We list nearly all repair costs for
+                  the the iPhones we offer service on. Should you not be able
+                  to find your repair listed, please get in touch and we’ll be
+                  able to advise further. For liquid damage devices, or no
+                  power devices we wouldn’t be able to provide a repair quote
+                  until after an inspection.
+                </dd>
+              </div>
+
+              <div>
+                <dt className="text-lg leading-6 font-medium text-gray-900">
+                  How long will it take?
+                </dt>
+                <dd className="mt-2 text-base text-gray-500">
+                  We understand how important iPhones have become, so we aim
+                  to complete all repairs in the same day, usually the same
+                  hour. A drop off repair for your iPhone is the quickest way
+                  to get your device repaired, and we usually complete nearly
+                  all iPhone repairs in under 30 minutes. Should you want to
+                  mail in instead, we aim to repair and dispatch the device
+                  back to you within the same day.
+                </dd>
+              </div>
+
+              <div>
+                <dt className="text-lg leading-6 font-medium text-gray-900">
+                  What to expect?
+                </dt>
+                <dd className="mt-2 text-base text-gray-500">
+                  We offer a wide range of repairs for all iPhone models,
+                  however should a repair not be possible on your device you
+                  can collect your iPhone with no fee or just the return
+                  postage fee if you’ve mailed in. Although we don’t routinely
+                  erase any device during repair, we do recommend you take a
+                  backup of your data before having your device repaired.
+                </dd>
+              </div>
+
+              <div>
+                <dt className="text-lg leading-6 font-medium text-gray-900">
+                  Screen Replacements
+                </dt>
+                <dd className="mt-2 text-base text-gray-500">
+                  iPhone screen replacements are our most common repair, we
+                  can repair most screens within around 20 minutes. We aim to
+                  keep all models of display in stock so you can pop into our
+                  Leeds store for a quick replacement without an appointment.
+                </dd>
+              </div>
+
+              <div>
+                <dt className="text-lg leading-6 font-medium text-gray-900">
+                  Battery Replacements
+                </dt>
+                <dd className="mt-2 text-base text-gray-500">
+                  iPhone batteries have a limited life span, this is usually
+                  around 1000 full charges. If your battery no longer gets you
+                  through the day we can offer a quick replacement, usually
+                  within 15 minutes. We keep nearly all batteries in stock, so
+                  you can stop by for a quick replacement at our Leeds store.
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
 
       <section className="py-12 bg-gray-50 overflow-hidden md:py-20 lg:py-24">
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
