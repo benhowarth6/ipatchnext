8aa728ff0c08953ef4c953eadab11ee376f3c360
Ben Howarth <benhowarth6@gmail.com>
2021-11-03 11:38:18 +0000

    macs

--- pages/mac-repairs/macbook-pro-repairs-b.js
+++ pages/mac-repairs/macbook-pro-repairs-b.js
@@ -90,7 +90,7 @@ export default function Home() {
                                                     selected
                                                         ? 'border-blue-500 text-blue-600'
                                                         : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                                                    'whitespace-nowrap py-6 border-b-2 font-medium text-sm'
+                                                    'whitespace-nowrap py-6 border-b-2 font-medium text-base'
                                                 )
                                             }
                                         >
