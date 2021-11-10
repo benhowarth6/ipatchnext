95d0d6125dc6fc16c7e2673c5b1672676f91d256
Ben Howarth <benhowarth6@gmail.com>
2021-11-10 10:28:57 +0000

    update find-repair.js

--- pages/find-repair.js
+++ pages/find-repair.js
@@ -78,42 +78,40 @@ export default function Example() {
 
         <main>
           {/* Category section */}
-          <section aria-labelledby="category-heading" className="py-24 sm:py-32 xl:max-w-7xl xl:mx-auto xl:px-8">
-            <div className="px-4 sm:px-6 sm:flex sm:items-center sm:justify-between lg:px-8 xl:px-0">
-              <h2 id="category-heading" className="text-2xl font-extrabold tracking-tight text-gray-900">
-                Devices we repair
-              </h2>
-            </div>
+          <div className="bg-white">
+            <div className="py-16 sm:py-24 xl:max-w-7xl xl:mx-auto xl:px-8">
+              <div className="px-4 sm:px-6 sm:flex sm:items-center sm:justify-between lg:px-8 xl:px-0">
+                <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Devices we repair</h2>
+              </div>
 
-            <div className="mt-4 flow-root">
-              <div className="-my-2">
-                <div className="box-content py-2 relative h-80 overflow-x-auto xl:overflow-visible">
-                  <div className="absolute min-w-screen-xl px-4 flex space-x-8 sm:px-6 lg:px-8 xl:relative xl:px-0 xl:space-x-0 xl:grid xl:grid-cols-5 xl:gap-x-8">
-                    {categories.map((category) => (
-                      <Link href={category.href}>
-                      <a
-                        key={category.name}
-                        className="relative w-56 h-80 rounded-lg p-6 flex flex-col overflow-hidden hover:opacity-75 xl:w-auto"
-                      >
-                        <span aria-hidden="true" className="absolute inset-0">
-                          <img src={category.imageSrc} alt="" className="w-full h-full object-center object-cover" />
-                        </span>
-                        <span
+              <div className="mt-8 grid grid-cols-2 gap-y-12 px-6 gap-x-6 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-5 xl:gap-x-8">
+                {categories.map((category) => (
+                  <Link href={category.href}>
+                  <a key={category.name} className="rounded-lg overflow-hidden hover:opacity-75">
+                    <div className="relative">
+                      <div className="relative w-full h-80 rounded-lg overflow-hidden ">
+                        <img
+                          src={category.imageSrc}
+                          className="w-full h-full object-center object-cover"
+                        />
+                      </div>
+                      <div className="absolute top-0 inset-x-0 h-80 rounded-lg p-4 items-end grid justify-items-center text-center overflow-hidden">
+                        <div
                           aria-hidden="true"
                           className="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50"
                         />
-                        <span className="relative mt-auto text-center text-xl font-bold text-white">{category.name}</span>
-                      </a>
-                      </Link>
-                    ))}
-                  </div>
-                </div>
+                        <p className="relative text-xl font-bold text-white">{category.name}</p>
+                      </div>
+                    </div>
+                  </a>
+                  </Link>
+                ))}
               </div>
             </div>
-          </section>
+          </div>
 
           <Incentive />
-          
+
         </main>
         <Footer />
       </div>
