9539f002468d10c33d4ac11e9b536f18659cb649
Ben Howarth <benhowarth6@gmail.com>
2021-12-01 23:39:25 +0000

    enable JIT

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,13 @@
 import Link from 'next/link'
+import Head from 'next/head'
 import { ChevronRightIcon } from '@heroicons/react/solid'
-import { BookmarkAltIcon, BookOpenIcon, RssIcon, ViewListIcon } from '@heroicons/react/outline'
+import { DeviceMobileIcon, DeviceTabletIcon, DesktopComputerIcon, PhoneIcon } from '@heroicons/react/outline'
 
 const links = [
-  { title: 'iPhone Repairs', href:'/iphone-repairs', description: 'Find and view the repairs we offer on all models of iPhone', icon: BookOpenIcon },
-  { title: 'iPad Repairs', href:'/ipad-repairs', description: 'Find and view the repairs we offer on all models of iPad', icon: ViewListIcon },
-  { title: 'Mac Repairs', href:'/mac-repairs', description: 'Find and view the repairs we offer on various Mac\'s', icon: BookmarkAltIcon },
-  { title: 'Contact', href:'/contact', description: 'Find out how you can get in touch with us', icon: RssIcon },
+  { title: 'iPhone Repairs', href: '/iphone-repairs', description: 'Find and view the repairs we offer on all models of iPhone', icon: DeviceMobileIcon },
+  { title: 'iPad Repairs', href: '/ipad-repairs', description: 'Find and view the repairs we offer on all models of iPad', icon: DeviceTabletIcon },
+  { title: 'Mac Repairs', href: '/mac-repairs', description: 'Find and view the repairs we offer on various Mac\'s', icon: DesktopComputerIcon },
+  { title: 'Contact', href: '/contact', description: 'Find out how you can get in touch with us', icon: PhoneIcon },
 ]
 const social = [
   {
@@ -48,75 +49,86 @@ const social = [
 
 export default function Example() {
   return (
-    <div className="bg-white">
-      <main className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex-shrink-0 pt-16">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="/logo.svg"
-            alt="Workflow"
-          />
-        </div>
-        <div className="max-w-xl mx-auto py-16 sm:py-24">
-          <div className="text-center">
-            <p className="text-sm font-semibold text-blue-600 uppercase tracking-wide">404 error</p>
-            <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
-              This page does not exist.
-            </h1>
-            <p className="mt-2 text-lg text-gray-500">The page you are looking for could not be found.</p>
+    <>
+      <Head>
+        <title>Error 404 - Page not found | iPatch</title>
+        <meta
+          name="description"
+          content="Error 404 - Page not found"
+          key="desc"
+        />
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
+      <div className="bg-white">
+        <main className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex-shrink-0 pt-16">
+            <img
+              className="mx-auto h-10 w-auto"
+              src="/logo.svg"
+              alt="Workflow"
+            />
           </div>
-          <div className="mt-12">
-            <h2 className="text-sm font-semibold text-gray-500 tracking-wide uppercase">Popular pages</h2>
-            <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
-              {links.map((link, linkIdx) => (
-                <li key={linkIdx} className="relative py-6 flex items-start space-x-4">
-                  <div className="flex-shrink-0">
-                    <span className="flex items-center justify-center h-12 w-12 rounded-lg bg-gray-50">
-                      <link.icon className="h-6 w-6 text-gray-700" aria-hidden="true" />
-                    </span>
-                  </div>
-                  <div className="min-w-0 flex-1">
-                    <h3 className="text-base font-medium text-gray-900">
-                      <span className="rounded-sm focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-gray-500">
-                        <Link href={link.href}>
-                        <a className="focus:outline-none">
-                          <span className="absolute inset-0" aria-hidden="true" />
-                          {link.title}
-                        </a>
-                        </Link>
+          <div className="max-w-xl mx-auto py-16 sm:py-24">
+            <div className="text-center">
+              <p className="text-sm font-semibold text-blue-600 uppercase tracking-wide">404 error</p>
+              <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                This page does not exist.
+              </h1>
+              <p className="mt-2 text-lg text-gray-500">The page you are looking for could not be found.</p>
+            </div>
+            <div className="mt-12">
+              <h2 className="text-sm font-semibold text-gray-500 tracking-wide uppercase">Popular pages</h2>
+              <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
+                {links.map((link, linkIdx) => (
+                  <li key={linkIdx} className="relative py-6 flex items-start space-x-4">
+                    <div className="flex-shrink-0">
+                      <span className="flex items-center justify-center h-12 w-12 rounded-lg bg-gray-50">
+                        <link.icon className="h-6 w-6 text-gray-700" aria-hidden="true" />
                       </span>
-                    </h3>
-                    <p className="text-base text-gray-500">{link.description}</p>
-                  </div>
-                  <div className="flex-shrink-0 self-center">
-                    <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                  </div>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-8">
-              <Link href="/">
-              <a className="text-base font-medium text-blue-500 hover:text-blue-600">
-                Or go back home<span aria-hidden="true"> &rarr;</span>
-              </a>
-              </Link>
+                    </div>
+                    <div className="min-w-0 flex-1">
+                      <h3 className="text-base font-medium text-gray-900">
+                        <span className="rounded-sm focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-gray-500">
+                          <Link href={link.href}>
+                            <a className="focus:outline-none">
+                              <span className="absolute inset-0" aria-hidden="true" />
+                              {link.title}
+                            </a>
+                          </Link>
+                        </span>
+                      </h3>
+                      <p className="text-base text-gray-500">{link.description}</p>
+                    </div>
+                    <div className="flex-shrink-0 self-center">
+                      <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-8">
+                <Link href="/">
+                  <a className="text-base font-medium text-blue-500 hover:text-blue-600">
+                    Or go back home<span aria-hidden="true"> &rarr;</span>
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-      <footer className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="border-t border-gray-200 py-12 text-center md:flex md:justify-between">
-          <p className="text-base text-gray-400">&copy; iPatch, Inc. All rights reserved.</p>
-          <div className="mt-6 flex justify-center space-x-8 md:mt-0">
-            {social.map((item, itemIdx) => (
-              <a key={itemIdx} href={item.href} target="_blank" className="inline-flex text-gray-400 hover:text-gray-500">
-                <span className="sr-only">{item.name}</span>
-                <item.icon className="h-6 w-6" aria-hidden="true" />
-              </a>
-            ))}
+        </main>
+        <footer className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="border-t border-gray-200 py-12 text-center md:flex md:justify-between">
+            <p className="text-base text-gray-400">&copy; iPatch, Inc. All rights reserved.</p>
+            <div className="mt-6 flex justify-center space-x-8 md:mt-0">
+              {social.map((item, itemIdx) => (
+                <a key={itemIdx} href={item.href} target="_blank" className="inline-flex text-gray-400 hover:text-gray-500">
+                  <span className="sr-only">{item.name}</span>
+                  <item.icon className="h-6 w-6" aria-hidden="true" />
+                </a>
+              ))}
+            </div>
           </div>
-        </div>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </>
   )
 }
