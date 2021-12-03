964dbc18f61c1afbb8e183d0cc0524f89a787736
Ben Howarth <benhowarth6@gmail.com>
2021-12-03 15:35:16 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import 'tailwindcss/tailwind.css'
+import '../styles/tailwind.css'
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import * as Fathom from 'fathom-client';
